ed085715db0eead6ececdb58b4f7337653f40a0d
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-11-17 12:02:48 -0500

    Fix update site name capitalization

--- neuroanatomy_deploy.sh
+++ neuroanatomy_deploy.sh
@@ -12,7 +12,7 @@ echo "====== Uploading to neuroanatomy-unstable update site ======"
 cd Fiji.app
 #curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
 chmod a+x ../upload-site-simple.sh
-../upload-site-simple.sh neuroanatomy-unstable Kharrington
+../upload-site-simple.sh Neuroanatomy-Unstable Kharrington
 
 
 # Could build a Fiji with neuroanatomy pre-installed
