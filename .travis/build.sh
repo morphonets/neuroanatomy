999faa0f92f849538d78167c4f22816940e74fec
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2020-04-30 22:22:48 -0400

    Switch travis build scripts

--- .travis/build.sh
+++ .travis/build.sh
@@ -11,8 +11,8 @@ fi
 UPLOAD_TO_UPDATE_SITE=true
 
 # Perform the main build.
-#curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh &&
-curl -fsLO https://gist.githubusercontent.com/kephale/fe09a1326c02d5e40626675476586cb7/raw/b3cdec5678d4d24b6efa8328620a019b2753a144/travis-build.sh &&
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh &&
+#curl -fsLO https://gist.githubusercontent.com/kephale/fe09a1326c02d5e40626675476586cb7/raw/b3cdec5678d4d24b6efa8328620a019b2753a144/travis-build.sh &&
 sh travis-build.sh $encrypted_eb7aa63bf7ac_key $encrypted_eb7aa63bf7ac_iv &&
 
 # Upload release version to the ImageJ update site.
